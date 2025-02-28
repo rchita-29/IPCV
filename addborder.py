b2f616073c734dd4180988dcd54b42c9f0da1e9f
rchita-29 <127950173+rchita-29@users.noreply.github.com>
2025-02-28 09:34:26 +0530

    Add files via upload

---
+++ addborder.py
@@ -0,0 +1,19 @@
+# Python program to explain cv2.copyMakeBorder() method 
+
+# importing cv2 
+import cv2 
+
+# path 
+path = r'lotus.jpg'
+
+# Reading an image in default mode 
+image = cv2.imread(path) 
+
+# Window name in which image is displayed 
+window_name = 'lotus'
+
+# Using cv2.copyMakeBorder() method 
+image = cv2.copyMakeBorder(image, 10, 10, 10, 10, cv2.BORDER_CONSTANT, None, value = 0) 
+
+# Displaying the image 
+cv2.imshow(window_name, image) 
